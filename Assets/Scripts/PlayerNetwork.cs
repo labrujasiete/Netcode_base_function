ce32d70343e1f4ee259bcc0c25a83cf044b0fe98
laBruja-Team <labrujasiete@gmail.com>
2023-03-23 11:26:55 -0700

    Update PlayerNetwork.cs
    keep working

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,27 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
 public class PlayerNetwork : NetworkBehaviour
 {
     
-    private NetworkVariable<int> randomNumber = new NetworkVariable<int>(1,NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
-    
     [SerializeField] private float movementSpeed = 7f;
     [SerializeField] private float rotationSpeed = 500f;
+
+    private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(new MyCustomData{
+        _int = 56,
+        _bool = true,
+    },NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    public struct MyCustomData : INetworkSerializable{
+        public int _int;
+        public bool _bool;
+        public FixedString128Bytes message;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+            serializer.SerializeValue(ref message);
+        }
+    }
+
+    public override void OnNetworkSpawn(){
+        randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) => {
+            Debug.Log(OwnerClientId + "; " + newValue._int + "; " + newValue._bool + "; " + newValue.message);
+        };
+    }
+    
+
 
 
     // Update is called once per frame
     void Update()
     {
 
-        Debug.Log(OwnerClientId + "; RandomNumber: " + randomNumber.Value);
-
         if(!IsOwner) return;
 
         if(Input.GetKeyDown(KeyCode.T)){
-            randomNumber.Value = Random.Range(0, 100);
+            TestServerRpc(new ServerRpcParams());
+            // randomNumber.Value = new MyCustomData{
+            //     _int = 10,
+            //     _bool = false,
+            //     message = "all your base is ours",
+            // };
         }
 
         float horizontalInput = Input.GetAxis("Horizontal");
@@ -35,8 +63,19 @@ public class PlayerNetwork : NetworkBehaviour
             Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
-
-
     }
+
+
+    [ServerRpc]
+    private void TestServerRpc(ServerRpcParams serverRpcParams){
+        Debug.Log("TestServerRpc " + OwnerClientId + "; " + serverRpcParams.Receive.SenderClientId);
+    }
+
+    [ClientRpc]
+    private void TestClientRpc(){
+        Debug.Log("TestClientRpc");
+    }
+    
+
 
 }
